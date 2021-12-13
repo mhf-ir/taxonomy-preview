0853425fc96478b50fd88070e8d579586b1f093f
Muhammad Hussein Fattahizadeh <m@mhf.ir>
2021-12-13 19:41:56 +0330

    feat: add google

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,7 @@
 const loadTrees = async () => {
   window.iptcTreeNumbers = 0;
   window.iabTreeNumbers = 0;
+  window.googleNumbers = 0;
   const a = new Promise((resolve) => {
     $.ajax({
       url: "./data/iptc_tree.json",
@@ -40,10 +41,30 @@ const loadTrees = async () => {
       },
     });
   });
-  await Promise.all([a, b]);
+  const c = new Promise((resolve) => {
+    $.ajax({
+      url: "./data/google_tree.json",
+      success(data) {
+        window.googleTree = $("#googleTree").tree({
+          data: data,
+          autoOpen: 0,
+          dragAndDrop: false,
+          onCreateLi: function (node, $li) {
+            // $li.find(".jqtree-element").append(`
+            //     <a href="#node-${node.id}" class="edit" data-node-id="${node.id}">🖊️<a>
+            //   `);
+            window.googleNumbers += 1;
+          },
+        });
+        resolve(true);
+      },
+    });
+  });
+  await Promise.all([a, b, c]);
   setTimeout(() => {
     $('#iptcTreeNumbers').html(window.iptcTreeNumbers);
     $('#iabTreeNumbers').html(window.iabTreeNumbers);
+    $('#googleNumbers').html(window.googleNumbers);
   }, 2000);
 
 };
